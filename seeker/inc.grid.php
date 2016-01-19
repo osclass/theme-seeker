ae493b3c98f660cd74f362f5d74a427665257b0d
Daniel Esteban <conejo@conejo.me>
2016-01-19 17:07:58 +0100

    fix

--- seeker/inc.grid.php
+++ seeker/inc.grid.php
@@ -21,7 +21,7 @@
 $class = __get('seeker_class');
 View::newInstance()->_exportVariableToView('seeker_class', $class=='odd'?'even':'odd');
 ?>
-<tr class="<?php echo $class; ?>">
+<tr class="<?php osc_run_hook("highlight_class"); ?> <?php echo $class; ?>">
 <td class="title">
 <strong><a href="<?php echo osc_item_url() ; ?>"><?php echo osc_item_title() ; ?></a></strong>
 </td>
@@ -30,6 +30,6 @@ View::newInstance()->_exportVariableToView('seeker_class', $class=='odd'?'even':
 </td>
 <td class="date"><?php echo osc_format_date(osc_item_pub_date() ) ; ?></td>
 </tr>
-<tr class="<?php echo $class; ?> description">
+<tr class="<?php osc_run_hook("highlight_class"); ?> <?php echo $class; ?> description">
 <td colspan="3"><?php echo strip_tags(osc_highlight( osc_item_description(), 250)) ; ?></td>
 </tr>
